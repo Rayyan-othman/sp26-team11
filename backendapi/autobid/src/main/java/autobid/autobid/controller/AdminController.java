c9af3f4d128b789ca7bb12027c511c75843a3be6
rayyan othman <raothman@uncg.edu>
2026-03-19 10:30:41 -0400

    all files working properly

--- backendapi/autobid/src/main/java/autobid/autobid/controller/AdminController.java
+++ backendapi/autobid/src/main/java/autobid/autobid/controller/AdminController.java
@@ -1,21 +1,27 @@
 package autobid.autobid.controller;
 
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import autobid.autobid.dto.AdminStatsResponse;
 import autobid.autobid.entity.Review;
 import autobid.autobid.entity.ServiceEntity;
 import autobid.autobid.entity.User;
 import autobid.autobid.service.AdminService;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/api/admin")
 public class AdminController {
 
     private final AdminService adminService;
-
     public AdminController(AdminService adminService) {
         this.adminService = adminService;
     }
