1bbd4a8a5232cc197226fc0b0b63a3f31f469c15
Rayyan Othman <raothman@uncg.edu>
2026-03-24 10:28:34 -0400

    Revert "GH-5"

--- backend-api/src/main/java/com/backendapi/autobid/CustomerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.backendapi.autobid;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CustomerRepository extends JpaRepository<Customer, Long> {
-    Customer findByEmail(String email);
-}
--- backend-api/src/main/java/com/backendapi/autobid/SubscriptionRepository.java
+++
@@ -1,6 +0,0 @@
-package com.backendapi.autobid;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SubscriptionRepository extends JpaRepository<Subscription, Long> {
-}
