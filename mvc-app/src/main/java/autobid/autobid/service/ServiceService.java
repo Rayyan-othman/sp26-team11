aa8da8acc2bfcc715863ceaa3753596909bc0e44
rayyan othman <raothman@uncg.edu>
2026-04-21 12:19:57 -0400

    new dashboard fixing reveiw and sertvices data fetching

--- mvc-app/src/main/java/autobid/autobid/service/ServiceService.java
+++ mvc-app/src/main/java/autobid/autobid/service/ServiceService.java
@@ -2,6 +2,7 @@ package autobid.autobid.service;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
+
 import autobid.autobid.entity.ServiceEntity;
 import autobid.autobid.repository.ServiceRepository;
 @Service
